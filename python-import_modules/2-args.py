13ce4723a4cfada5400574af740543b557a9b75a
blythelockhart <blythe.lockhart@att.net>
2023-09-07 18:23:03 -0700

    coded files 0-, 1-, and 2-

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+import sys
+argv = sys.argv[1:]
+num_args = len(argv)
+if num_args == 0:
+    print('0 arguments.')
+elif num_args == 1:
+    print('1 argument:')
+    print('1: {}'.format(argv[1])) 
+else:
+    print('{} arguments:'.format(num_args))
+    for i in range(num_args):
+        print('{}: {}'.format(i, argv[i]))
