be81d64e7178d22caa2d50f47b0fae9a12856d24
blythelockhart <blythe.lockhart@att.net>
2023-09-07 18:38:31 -0700

    coded files 3-, 4-, and 5-

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-import sys
-argv = sys.argv[1:]
-num_args = len(argv)
-if num_args == 0:
-    print('0 arguments.')
-elif num_args == 1:
-    print('1 argument:')
-    print('1: {}'.format(argv[1])) 
-else:
-    print('{} arguments:'.format(num_args))
-    for i in range(num_args):
-        print('{}: {}'.format(i, argv[i]))
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 0:
+        print('0 arguments.')
+    elif num_args == 1:
+        print('1 argument:')
+        print('1: {}'.format(argv[1])) 
+    else:
+        print('{} arguments:'.format(num_args))
+        for i in range(num_args):
+            print('{}: {}'.format(i, argv[i]))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    arg_sum = 0
+    for i in range(1, len(sys.argv)):
+        arg_sum += int(sys.argv[i])
+    print('{}'.format(arg_sum))
