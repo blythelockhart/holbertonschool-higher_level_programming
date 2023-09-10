8fc2950c5877222c99c0f2134373cd1332522161
blythelockhart <blythe.lockhart@att.net>
2023-09-09 23:47:30 -0700

    coded files 3-, 4-, and 5-

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ''
+    for i in range(len(my_string)):
+        if mystring[i] != 'c' or my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string
