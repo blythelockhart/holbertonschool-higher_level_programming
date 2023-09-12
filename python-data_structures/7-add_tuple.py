36038fc8ed6801f2e9be6b5b9ec10bd1251cc6bf
blythelockhart <blythe.lockhart@att.net>
2023-09-12 14:06:06 -0700

    corrected issues with files 9-, 8-, 7-, and 6-

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -13,4 +13,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
     apple_a = tuple_a[0] + tuple_b[0]
     apple_b = tuple_a[1] + tuple_b[1]
-    return(apple_a, apple_b)
+    return apple_a, apple_b
