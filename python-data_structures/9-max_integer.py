0a2a29fc27cd0fafadcb10464397bad301bc0f6d
blythelockhart <blythe.lockhart@att.net>
2023-09-12 13:37:44 -0700

    coded files 9-, 10-, 11-, and 12-

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    keep = 0
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i > keep:
+            keep = i
+    return keep
