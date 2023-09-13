8660918355773f944494c81b4ac33a8f9e120621
blythelockhart <blythe.lockhart@att.net>
2023-09-12 18:30:28 -0700

    coded files 3-, 4-, and 5-

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new_s = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                new_s.append(i)
+    return new_s
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+              del set_1[i]
+              del set_2[j]
+    return set_1, set_2
