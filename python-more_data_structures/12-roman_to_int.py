cb868b4533cbdde336161837a5c6cb2048b3b93e
blythelockhart <blythe.lockhart@att.net>
2023-09-15 00:08:56 -0700

    coded file 12-

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+dict_c = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+}
 def roman_to_int(roman_string):
+    num = 0
+    prev_value = 0
+    for char in roman_string[::-1]:
+        value = roman_dict[char]
+        if value < prev_value:
+            num -= value
+        else:
+            num += value
+        prev_value = value
+    return num
