f4b32a4b03a651ec1565d58d71527ca9e9fc8e81
blythelockhart <blythe.lockhart@att.net>
2023-09-20 21:40:28 -0700

    added rectangle-comparison function to files 8- and 9-

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -91,3 +91,17 @@ class Rectangle:
         """Bids rectangle farewell when deleted."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    def bigger_or_equal(rect_1, rect_2):
+        """Return with the bigger rectangle.
+        Args:
+            rect_1 (Rectangle): Rectangle to be compared.
+            rect_2 (Rectangle): Rectangle to be compared.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        return rect_2
