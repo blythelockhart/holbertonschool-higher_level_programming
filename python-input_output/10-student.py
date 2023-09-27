a760f827de4f3461e23e43eb178a7059d723d332
blythelockhart <blythe.lockhart@att.net>
2023-09-26 21:00:41 -0700

    corrected issues with files 9- and 10-

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,9 +11,9 @@ class Student:
             last_name (str): The student's last name.
             age (int): The student's age.
         """
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """ Return the dictionary representation.
