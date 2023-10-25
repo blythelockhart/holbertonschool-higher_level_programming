fc3ad1ba1777f483e24d2aa0d156d4ec3e0b4974
blythelockhart <blythe.lockhart@att.net>
2023-10-24 18:12:52 -0700

    corrected file 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,9 @@ import MySQLdb
 
 if __name__ == "__main__":
     """ Lists all States from the database 'hbtn_0e_0_usa'. """
-    database = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    database = MySQLdb.connect(user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3])
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
