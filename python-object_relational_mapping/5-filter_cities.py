4321840687ffd993d60d0bd10d44c70e2cfd7303
blythelockhart <blythe.lockhart@att.net>
2023-10-24 19:52:12 -0700

    corrected file 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ and lists all cities of that state.
 
 
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
@@ -14,18 +14,18 @@ if __name__ == '__main__':
     Takes in the name of a state as an argument and lists all
     cities of that state, using the database 'hbtn_0e_4_usa'.
     """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
     database = MySQLdb.connect(user=username,
                                passwd=password,
                                db=database_name,
                                host="localhost",
                                port=3306)
     cursor = database.cursor()
-    cursor.execute("SELECT name FROM cities WHERE states_id IN (SELECT id \
-                   FROM states WHERE name = %s ORDER BY id", (state_name,))
+    cursor.execute("SELECT name FROM cities WHERE state_id IN (SELECT id \
+                   FROM states WHERE name = %s) ORDER BY id", (state_name,))
     cities = cursor.fetchall()
 
     for city in cities:
