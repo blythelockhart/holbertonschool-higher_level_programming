ed181ff9c6c535e3ccf8775db7ceb56bd1f2323f
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-28 12:06:45 -0800

    dorrected file 6 inheriting from correct file

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
